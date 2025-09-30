741243aa2c7bc3f334af1cd6d4a3149868fc5bdd
codeconnoisseur-channels <channelsokunade@gmail.com>
2025-09-30 12:12:26 +0100

    cronjob frontend

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+require("./config/database");
+const cors = require("cors");
+const app = express();
+app.use(
+  cors({
+    origin: ["http://localhost:7000", "https://myapp.com"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+const PORT = process.env.PORT;
+
+const userRouter = require("./router/userRouter");
+const { cronSchedule } = require("./utils/cronJob");
+app.use(userRouter);
+
+cronSchedule();
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
